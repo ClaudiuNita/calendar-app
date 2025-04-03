83434b47a865ebe43d5673ef2a20cc13c223f51e
ClaudiuNita <98160469+ClaudiuNita@users.noreply.github.com>
2025-04-03 20:52:34 +0300

    Delete calendar-api directory

--- calendar-api/src/main/java/org/example/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
